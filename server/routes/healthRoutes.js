9d591299e40de144cf46b8a4c569d062c09a5970
rajveer3x <panwarrajveer07@gmail.com>
2026-04-07 20:48:25 +0530

    initilized

--- server/routes/healthRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getHealth } from '../controllers/healthController.js';
-
-const router = express.Router();
-
-// Define health check route
-router.get('/', getHealth);
-
-export default router;
